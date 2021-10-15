1cee2c00854b042348a4d410e34ee0d7fb745ceb
nicolasquinchia <nicolasquinchia@gmail.com>
2021-10-14 23:40:20 -0500

    merge sort

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+/*Librarys standars*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prints an array of integers */
+void print_array(const int *array, size_t size);
+
+
+void merge_sort(int *array, size_t size);
+
+#endif /* !_SORT_H_*/
